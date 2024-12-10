950bfa4a3db57a71f65b8e671ebe4601f4256c89
virtualxiaoman <2958694743@qq.com>
2024-12-10 18:05:17 +0800

    更新markdown

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 azusa是白州梓的名字，我很喜欢她。Vanitas Vanitatum，一切都是虚空，但也不能放弃抵抗。
 
 希望我的博客能成为一朵坚定向光的花，也为你带来一点温暖。
+
+博客构建方法：
+```bash
+cd azusa_front
+npm run build
+npm run deploy 
+```
--- azusa_front/src/views/HomePage/blogpost.vue
+++ azusa_front/src/views/HomePage/blogpost.vue
@@ -1,45 +1,52 @@
 <template>
-    <div class="blog-section">
+  <div class="blog-section">
     <div class="blog-post" @click="goToArticle('/article/深度学习')">
-        <img src="/article_cover/深度学习.jpeg" alt="博客图片">
-        <div class="post-info">
-            <h3>深度学习</h3>
-            <p>文章内容简介...</p>
-        </div>
+      <img src="/article_cover/深度学习.jpeg" alt="博客图片">
+      <div class="post-info">
+        <h3>深度学习</h3>
+        <p>文章内容简介...</p>
+      </div>
     </div>
     <div class="blog-post" @click="goToArticle('/article/机器学习')">
-        <img src="/article_cover/机器学习.jpg" alt="博客图片">
-        <div class="post-info">
-            <h3>机器学习</h3>
-            <p>文章内容简介...</p>
-        </div>
+      <img src="/article_cover/机器学习.jpg" alt="博客图片">
+      <div class="post-info">
+        <h3>机器学习</h3>
+        <p>文章内容简介...</p>
+      </div>
+    </div>
+    <div class="blog-post" @click="goToArticle('/article/机器学习实践')">
+      <img src="/article_cover/机器学习实践.png" alt="博客图片">
+      <div class="post-info">
+        <h3>计算机视觉</h3>
+        <p>文章内容简介...</p>
+      </div>
     </div>
     <div class="blog-post" @click="goToArticle('/article/推荐系统')">
-        <img src="/article_cover/推荐系统.jpeg" alt="博客图片">
-        <div class="post-info">
-            <h3>推荐系统</h3>
-            <p>文章内容简介...</p>
-        </div>
+      <img src="/article_cover/推荐系统.jpeg" alt="博客图片">
+      <div class="post-info">
+        <h3>推荐系统</h3>
+        <p>文章内容简介...</p>
+      </div>
     </div>
     <div class="blog-post" @click="goToArticle('/article/自然语言处理')">
-        <img src="/article_cover/自然语言处理.jpeg" alt="博客图片">
-        <div class="post-info">
-            <h3>自然语言处理</h3>
-            <p>文章内容简介...</p>
-        </div>
+      <img src="/article_cover/自然语言处理.jpeg" alt="博客图片">
+      <div class="post-info">
+        <h3>自然语言处理</h3>
+        <p>文章内容简介...</p>
+      </div>
     </div>
     <div class="blog-post" @click="goToArticle('/article/choice')">
-        <img src="/article_cover/其他文章.png" alt="博客图片">
-        <div class="post-info">
-            <h3>其他文章</h3>
-            <p>文章内容简介...</p>
-        </div>
+      <img src="/article_cover/其他文章.png" alt="博客图片">
+      <div class="post-info">
+        <h3>其他文章</h3>
+        <p>文章内容简介...</p>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
+import {useRouter} from 'vue-router';
 
 const router = useRouter();
 
@@ -49,38 +56,38 @@ const router = useRouter();
 //     router.push('/article/深度学习');
 // };
 const goToArticle = (path: string) => {
-    router.push(`${import.meta.env.BASE_URL}${path.replace(/^\//, '')}`); // 确保路径前缀正确(防止//article/...的情况)
+  router.push(`${import.meta.env.BASE_URL}${path.replace(/^\//, '')}`); // 确保路径前缀正确(防止//article/...的情况)
 };
 
 </script>
 
 <style scoped>
 .blog-post {
-    display: flex;
-    margin-bottom: 2rem;
-    border: 1px solid #ddd;
-    border-radius: 8px;
-    overflow: hidden;
+  display: flex;
+  margin-bottom: 2rem;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  overflow: hidden;
 }
 
 .blog-post img {
-    width: 25%;
-    height: 175px;
-    object-fit: cover;
+  width: 25%;
+  height: 175px;
+  object-fit: cover;
 }
 
 .post-info {
-    padding: 1rem;
+  padding: 1rem;
 }
 
 .post-info h3 {
-    margin: 0;
-    font-size: 24px;
-    color: #409EFF;
+  margin: 0;
+  font-size: 24px;
+  color: #409EFF;
 }
 
 .post-info p {
-    margin: 12px 0;
-    color: #73767a;
+  margin: 12px 0;
+  color: #73767a;
 }
 </style>
